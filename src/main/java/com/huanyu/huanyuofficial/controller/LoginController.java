e811cf74074b2ec546228c206521b25d23d63d8a
pengjiazhen <jzpenga@gmail.com>
2018-12-28 18:38:13 +0800

    更改数据库

--- src/main/java/com/huanyu/huanyuofficial/controller/LoginController.java
+++ src/main/java/com/huanyu/huanyuofficial/controller/LoginController.java
@@ -18,7 +18,6 @@ public class LoginController {
     @ApiOperation("登陆")
     @PostMapping
     public BaseResponse<User> login(@RequestBody LoginParam loginParam){
-        System.out.println(2/0);
         return loginService.login(loginParam);
     }
 }
